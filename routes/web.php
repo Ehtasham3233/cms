05089482b7ccdcf1649d7374016a38230c2a0410
ehtsham3233@gmail.com <you@example.com>
2022-11-05 20:45:41 +0500

    Controllers setting

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home, Basecontroller::home');
+Route::get('/services, Basecontroller@services');
+Route::get('/company, Basecontroller@company');
+Route::get('/contact_us, Basecontroller@contact_us');
+
