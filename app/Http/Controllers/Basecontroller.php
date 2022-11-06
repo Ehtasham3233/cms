3b2833ea0871620573d62a01eef1bc8ebf78faf3
Ehtsham3233 <ehtsham3233@gmail.com>
2022-11-06 21:02:03 +0500

    changed laptop

---
+++ app/Http/Controllers/Basecontroller.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Basecontroller extends Controller
+{
+    public function home()
+    {
+
+    }
+    public function services()
+    {
+
+    }
+    public function company()
+    {
+
+    }
+    public function contact_us()
+    {
+
+    }
+}
